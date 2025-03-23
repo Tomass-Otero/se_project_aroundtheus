d6d603a5138fd80d7efdbebdf7aa1597b67e44ae
Tomas Otero <tomass.gordillo@gmail.com>
2025-03-22 20:34:08 -0400

    Popup with image

--- src/scripts/Popup.js
+++ src/scripts/Popup.js
@@ -4,18 +4,29 @@ export default class Popup {
   }
 
   open() {
-    //open modal
+    this._popupElement.classList.add("modal_opened");
+    document.addEventListener("keydown", this._handleEscClose);
   }
 
   close() {
-    //close modal
+    this._popupElement.classList.remove("modal_opened");
+    document.removeEventListener("keydown", this._handleEscClose);
   }
 
-  _handleEscClose() {
-    //Esc modal
+  _handleEscClose(e) {
+    if (e.key === "Escape") {
+      this.close();
+    }
   }
 
   setEventListeners() {
-    //set Event Listeners
+    this._popupElement.addEventListener("mousedown", (e) => {
+      if (
+        e.target.classList.contains("modal__close") ||
+        e.target.classList.contains("modal_opened")
+      ) {
+        this.close();
+      }
+    });
   }
 }
---
+++ src/scripts/PopupWithImage.js
@@ -0,0 +1,14 @@
+import Popup from "./scripts/Popup.js";
+
+class PopupWithImage extends Popup {
+  constructor(popupSelector) {
+    super({ popupSelector });
+    this._imageEl = this._popupElement.querySelector(".modal__preview-image");
+    this._captionEl = this._popupElement.querySelector(".modal__image-caption");
+  }
+  open(name, link) {
+    this._imageEl.src = link;
+    this._captionEl.textContent = name;
+    super(open);
+  }
+}
